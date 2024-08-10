2ace26a23a6ea99f592d38782335d2a5ae09e3f0
CARDONA LUIS <91144533+CarLuis07@users.noreply.github.com>
2024-08-10 15:34:52 -0600

    update

--- catracho-pizzas-app/src/app/home/home.page.ts
+++ catracho-pizzas-app/src/app/home/home.page.ts
@@ -2,7 +2,9 @@
 import { Component } from '@angular/core';
 import { IonContent , IonCard, IonImg, IonButton, IonCardHeader, IonCardTitle, IonCardContent, IonButtons, IonIcon} from '@ionic/angular/standalone';
 import { addIcons } from 'ionicons';
-import { remove, addOutline } from 'ionicons/icons';
+import { remove, addOutline, cartOutline } from 'ionicons/icons';
+import { AlertController } from '@ionic/angular';
+
 
 @Component({
   selector: 'app-home',
@@ -11,8 +13,38 @@ import { remove, addOutline } from 'ionicons/icons';
   standalone: true,
   imports: [IonContent , IonCard, IonImg, IonButton, IonCardHeader, IonCardTitle, IonCardContent, IonButtons, IonIcon],
 })
+
 export class HomePage {
-  constructor() {
-    addIcons({ remove, addOutline });
+  
+  piz_cantidad: number
+  tittle:string=''
+
+  constructor(private AlertController:AlertController) {
+    
+    this.piz_cantidad=1
+    
+    addIcons({ remove, addOutline,cartOutline });
+  }
+  
+  handlerAdd(){
+    this.piz_cantidad+=1
+  }
+
+  handlerRem(){
+    if(this.piz_cantidad>1)
+    this.piz_cantidad-=1
+  }
+
+  async AlertOrdenar(){
+    
+    const alert= await this.AlertController.create(
+    {
+      header:'ORDEN DE COMIDA',
+      message: `Ha agregado correctamente ${this.piz_cantidad} pizza(s) a su carrito`,
+      buttons:['Aceptar']
+    }
+  )  
+  
+  await alert.present()
   }
 }
